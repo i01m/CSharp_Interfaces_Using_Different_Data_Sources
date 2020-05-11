7c0762eb078758799c1fe1a27b4f47080669205b
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 14:41:51 -0500

    first commit

---
+++ PersonRepository.Interface/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonRepository.Interface
+{
+    public interface IPersonRepository : IPersonReader
+    {                    
+        void AddPerson(Person newPerson);
+
+        void UpdatePerson(int id, Person updatedPerson);
+
+        void DeletePerson(int id);
+    }
+}
---
+++ PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository : IPersonReader
+    {
+        WebClient client = new WebClient();
+        string baseUri = "http://localhost:9874/api/people";
+
+
+        public IEnumerable<Person> GetPeople()
+        {
+            string result = client.DownloadString(baseUri);
+
+            IEnumerable<Person> people =
+                JsonConvert.DeserializeObject<IEnumerable<Person>>(result);
+
+            return people;
+        }
+
+        public Person GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }       
+    }
+}
